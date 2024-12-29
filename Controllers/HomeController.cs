00ac83378b141b993b6f406751e041bb32359da7
MUKESH KUMAR <mukeshbcc56@gmail.com>
2024-12-29 11:44:25 +1100

    Committ

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,12 +4,41 @@ using System.Data.SqlClient;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
+using MyMvcApp.Models;
+
 
 namespace MyMVCApp.Controllers
 {
     public class HomeController : Controller
     {
-        
+        [HttpPost]
+        public void InsertWrongAnswer([FromBody]WrongAnswers wrongAnswer)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string enableIdentityInsert = "SET IDENTITY_INSERT WrongAnswers ON;";
+                string query = "INSERT INTO WrongAnswers (Id, Question, Options, Answer, SoftDelete) VALUES (@Id, @Question, @Options, @Answer, @SoftDelete)";
+                string disableIdentityInsert = "SET IDENTITY_INSERT WrongAnswers OFF;";
+
+                SqlCommand enableCommand = new SqlCommand(enableIdentityInsert, connection);
+                SqlCommand command = new SqlCommand(query, connection);
+                SqlCommand disableCommand = new SqlCommand(disableIdentityInsert, connection);
+
+                command.Parameters.AddWithValue("@Id", wrongAnswer.Id);
+                command.Parameters.AddWithValue("@Question", "wrongAnswer.Question");
+                command.Parameters.AddWithValue("@Options", string.Join(";", wrongAnswer.Options));
+                command.Parameters.AddWithValue("@Answer", "wrongAnswer.Answer");
+                command.Parameters.AddWithValue("@SoftDelete", 1);
+
+                connection.Open();
+
+                enableCommand.ExecuteNonQuery();
+                command.ExecuteNonQuery();
+                disableCommand.ExecuteNonQuery();
+            }
+        }
+
+
         public class QuestionModel
         {
             public int Id { get; set; }
@@ -17,6 +46,30 @@ namespace MyMVCApp.Controllers
             public List<string> Options { get; set; }
             public string Answer { get; set; }
         }
+
+        public class WrongAnswers
+        {
+            public int Id { get; set; }
+            public string Question { get; set; }
+            public List<string> Options { get; set; }
+            public string Answer { get; set; }
+            public bool SoftDelete { get; set; }
+
+            public WrongAnswers()
+            {
+                Options = new List<string>();
+            }
+
+            public WrongAnswers(int id, string question, List<string> options, string answer, bool softDelete)
+            {
+                Id = id;
+                Question = question;
+                Options = options;
+                Answer = answer;
+                SoftDelete = softDelete;
+            }
+        }
+
         private readonly string connectionString = "Server=tcp:flashdbserver.database.windows.net,1433;Initial Catalog=flashcardsdb;Persist Security Info=False;User ID=admin123;Password=Mukesh123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
         // GET: Home
@@ -54,13 +107,28 @@ namespace MyMVCApp.Controllers
 
             return questions;
         }
-
+        
         [HttpPost]
-        public ActionResult SubmitAnswer(int id, string selectedOption)
+        public ActionResult SubmitAnswer([FromBody] WrongAnswers wrongAnswerData)
         {
-            var correct = CheckAnswer(id, selectedOption);
+            var correct = CheckAnswer(wrongAnswerData.Id, wrongAnswerData.Answer);
+
+            if (!correct)
+            {
+                InsertWrongAnswer(new WrongAnswers
+                {
+                    Id = wrongAnswerData.Id,
+                    Question = wrongAnswerData.Question,
+                    Options = wrongAnswerData.Options,
+                    Answer = wrongAnswerData.Answer,
+                   
+                });
+            }
+
             return Json(new { Correct = correct });
         }
+
+        
 
         private bool CheckAnswer(int id, string selectedOption)
         {
@@ -77,6 +145,4 @@ namespace MyMVCApp.Controllers
             }
         }
     }
-
-
 }
